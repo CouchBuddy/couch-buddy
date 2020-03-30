c44e599caf2d8780dbaeae9cd4c04f809002a6f1
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-03-30 16:13:20 +0200

    add tailwind

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
