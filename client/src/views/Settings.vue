8cba9e7076e9140b85c42d0a89b77884bc712264
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-04-05 16:31:47 +0200

    settings layout fix

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -8,7 +8,10 @@
       System Info
     </h2>
 
-    <div v-if="systemInfo">
+    <div
+      v-if="systemInfo"
+      class="mb-8"
+    >
       <progress
         v-if="systemInfo.diskSpace && systemInfo.mediaDirAvailable"
         min="0"
@@ -27,24 +30,18 @@
       <div>CouchBuddy v{{ systemInfo.version }}</div>
     </div>
 
-    <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          for="inline-full-name"
-        >
-          Library
-        </label>
-      </div>
-      <div class="md:w-2/3">
-        <button
-          class="bg-transparent border-2 px-4 py-2"
-          :disabled="scanningLibrary"
-          @click="scanLibrary()"
-        >
-          {{ scanningLibrary ? 'Scanning...' : 'Scan Library' }}
-        </button>
-      </div>
+    <div class="mb-8">
+      <h2 class="text-2xl mb-4">
+        Library
+      </h2>
+
+      <button
+        class="bg-transparent border-2 px-4 py-2"
+        :disabled="scanningLibrary"
+        @click="scanLibrary()"
+      >
+        {{ scanningLibrary ? 'Scanning...' : 'Scan Library' }}
+      </button>
     </div>
   </div>
 </template>
