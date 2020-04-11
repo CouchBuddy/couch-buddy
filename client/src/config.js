eb1856ce56ff8a6362c85abf4ebd3f5c4de39fb4
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-04-11 17:44:49 +0200

    set client api url for production

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,7 @@
+const isProduction = process.env.NODE_ENV
+
 export default {
   castReceiverAppId: null,
-  serverUrl: `${location.protocol}//${location.hostname}:3000`
+  isProduction,
+  serverUrl: isProduction ? '/' : `${location.protocol}//${location.hostname}:3000`
 }
