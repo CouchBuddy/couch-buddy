51a499f54d921d774f36f0864e96ecfb9b312982
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-04-13 16:40:16 +0200

    fix fts setup script

--- server/models/index.js
+++ server/models/index.js
@@ -23,45 +23,44 @@ async function sync () {
 
     // Check if FTS tables have been already created, all the next queries are safe, but the 'INSERT INTO' queries
     const [ ftsTableExists ] = await sequelize.query('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'movies_fts\';', { type: Sequelize.QueryTypes.SELECT })
-    if (ftsTableExists) {
-      return
-    }
 
     // Create a virtual table for FTS5 based on `movies` columns
     const searchFields = 'id, actors, director, plot, title'
     await sequelize.query(`CREATE VIRTUAL TABLE IF NOT EXISTS movies_fts USING fts5(${searchFields});`)
 
-    // Copy rows from the normal movies table to FTS movie table
-    await sequelize.query(`INSERT INTO movies_fts SELECT ${searchFields} FROM movies;`)
+    if (!ftsTableExists) {
+      // Copy rows from the normal movies table to FTS movie table
+      await sequelize.query(`INSERT INTO movies_fts SELECT ${searchFields} FROM movies;`)
 
-    // Set default columns weights for search order
-    await sequelize.query('INSERT INTO movies_fts(movies_fts, rank) VALUES(\'rank\', \'bm25(0.0, 1, 1, 3, 5)\');')
+      // Set default columns weights for search order
+      await sequelize.query('INSERT INTO movies_fts(movies_fts, rank) VALUES(\'rank\', \'bm25(0.0, 1, 1, 3, 5)\');')
+    }
 
     // Add triggers to keep movies tables in sync with FTS table
     // After Insert
     await sequelize.query(`
     CREATE TRIGGER IF NOT EXISTS movies_after_insert AFTER INSERT ON movies BEGIN
-      INSERT INTO movies_fts(docid, ${searchFields}) SELECT rowid, ${searchFields} FROM movies WHERE is_conflict = 0 AND encryption_applied = 0 AND new.rowid = notes.rowid;
+      INSERT INTO movies_fts(${searchFields}) SELECT ${searchFields} FROM movies WHERE new.id = movies.id;
     END;`)
 
     // Before Update
     await sequelize.query(`
     CREATE TRIGGER IF NOT EXISTS movies_fts_before_update BEFORE UPDATE ON movies BEGIN
-      DELETE FROM movies_fts WHERE docid=old.rowid;
+      DELETE FROM movies_fts WHERE id=old.id;
     END
     `)
 
     // After Update
     await sequelize.query(`
     CREATE TRIGGER IF NOT EXISTS movies_after_update AFTER UPDATE ON movies BEGIN
-      INSERT INTO movies_fts(docid, ${searchFields}) SELECT rowid, ${searchFields} FROM movies WHERE is_conflict = 0 AND encryption_applied = 0 AND new.rowid = notes.rowid;
+      INSERT INTO movies_fts(${searchFields}) SELECT ${searchFields} FROM movies WHERE new.id = movies.id;
     END
     `)
 
     // Before Delete
     await sequelize.query(`
     CREATE TRIGGER IF NOT EXISTS movies_fts_before_delete BEFORE DELETE ON movies BEGIN
-      DELETE FROM movies_fts WHERE docid=old.rowid;
+      DELETE FROM movies_fts WHERE id=old.id;
     END
     `)
 
