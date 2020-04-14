ac81db7f03066aa40ac7ba6ec227409dffad0a77
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-04-14 11:43:23 +0200

    add <x-input> base component

---
+++ client/src/components/base/Input.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex items-center mb-8 bg-gray-800">
+    <input
+      ref="input"
+      v-model="currentValue"
+      v-bind="$attrs"
+      class="flex-grow px-4 py-2 bg-transparent border-0"
+      @input="onInput"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [ String, Number ],
+      default: null
+    }
+  },
+  data () {
+    return {
+      currentValue: this.value,
+      valueWhenFocus: null
+    }
+  },
+  methods: {
+    onInput (val) {
+      this.$emit('input', this.currentValue)
+    }
+  }
+}
+</script>
