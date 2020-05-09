f6edccd5ae26d9fbc5596be9b0769991dafc412a
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-05-09 16:59:17 +0200

    port omdb api

--- server/src/services/omdb.ts
+++ server/src/services/omdb.ts
@@ -1,22 +1,68 @@
-const axios = require('axios')
+import axios from 'axios'
 
-const config = require('../config')
+import config from '../config'
+import Episode from '../models/Episode'
+import Movie from '../models/Movie'
 
-const ALLOWED_TYPES = [ 'episode', 'movie', 'series' ]
+interface SearchParams {
+  episode?: number;
+  imdbId?: string;
+  season?: number;
+  title?: string;
+  type: 'episode' | 'movie' | 'series';
+  year?: number;
+}
+
+interface OmdbQueryParams {
+  /**
+   * API Key
+   */
+  apikey: string;
+
+  /**
+   * TV Series Episode number
+   */
+  Episode?: number;
+
+  /**
+   * Imdb ID
+   */
+  i?: string;
+
+  /**
+   * TV Series Season number
+   */
+  Season?: number;
+
+  /**
+   * Title
+   */
+  t?: string;
+
+  /**
+   * Show type
+   */
+  type?: 'episode' | 'movie' | 'series';
+
+  /**
+   * Year
+   */
+  y?: number;
+}
 
 /**
  * Search a movie, episode or show on OMDb. The query accepts multiple parameters,
  * but if IMDB ID is provided (`imdbId` field), the other search params are ignored.
  *
- * @param {Object} search can have `imdbId`, `title`, `season` and `episode` properties
- * @return {Object} the search result with fields name converted lower pascalCase
+ * @param search
+ * @return the search result with fields name converted lower pascalCase
  *    (i.e.: Title becomes title) or null
  */
-module.exports = async function searchOmdb (search) {
-  let result = null
+export default async function searchOmdb (search: SearchParams) {
+  let result: Movie | Episode = null
 
   try {
-    const params = {
+    const params: OmdbQueryParams = {
       apikey: config.omdbApiKey
     }
 
@@ -26,7 +72,7 @@ module.exports = async function searchOmdb (search) {
     } else {
       if (search.title) { params.t = search.title }
       if (search.year) { params.y = search.year }
-      if (ALLOWED_TYPES.includes(search.type)) { params.type = search.type }
+      params.type = search.type
 
       // If it's an episode, add S and E to force looking for a series' episode.
       // This helps for issue #34, series and movie with the same name
@@ -37,7 +83,7 @@ module.exports = async function searchOmdb (search) {
     const response = await axios.get('http://www.omdbapi.com/', { params })
 
     if (!response.data.Error) {
-      result = {}
+      result = response.data.type === 'movie' ? new Movie() : new Episode()
 
       // Found info on OMDb, refactor object to match Movie/Episode model
       for (const k in response.data) {
@@ -45,14 +91,8 @@ module.exports = async function searchOmdb (search) {
           ? response.data[k] : null
       }
 
-      result.imdbId = result.imdbID
-      result.ratingImdb = result.imdbRating ? parseFloat(result.imdbRating) : null
-      result.ratingMetacritic = result.metascore ? parseInt(result.metascore) : null
-
-      const rt = result.ratings.find(x => x.Source === 'Rotten Tomatoes')
-      if (rt) {
-        result.ratingRottenTomatoes = parseInt(rt.Value)
-      }
+      result.imdbId = response.data.imdbID
+      result.rating = response.data.imdbRating ? parseFloat(response.data.imdbRating) : null
     } else {
       // Not found on OMDb
       console.warn('OMDb:', response.data.Error, search.title)
