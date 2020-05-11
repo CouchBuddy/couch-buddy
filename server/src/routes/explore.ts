7fca13c8c5ea675d30b25546b8c9525fc2fba448
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-05-11 16:52:15 +0200

    fix explore method

--- server/src/routes/explore.ts
+++ server/src/routes/explore.ts
@@ -5,5 +5,5 @@ import { allExtensions } from '../services/extensions'
 export async function searchMovies (ctx: Context) {
   const { search } = ctx.request.query
 
-  ctx.body = allExtensions[0].search(search)
+  ctx.body = allExtensions[0].explore(search)
 }
