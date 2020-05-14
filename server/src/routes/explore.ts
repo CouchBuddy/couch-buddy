14052f2afc40fe8c3711c11f89af43db2fa9e64c
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-05-14 16:38:13 +0200

    check if any explorer extension exists

--- server/src/routes/explore.ts
+++ server/src/routes/explore.ts
@@ -5,5 +5,11 @@ import { allExtensions } from '../services/extensions'
 export async function searchMovies (ctx: Context) {
   const { search } = ctx.request.query
 
+  if (!allExtensions.length) {
+    ctx.status = 404
+    ctx.body = 'Explore providers not found. Please install an Explore extension.'
+    return
+  }
+
   ctx.body = allExtensions[0].explore(search)
 }
