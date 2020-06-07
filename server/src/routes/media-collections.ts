f789bd5646afea1952d3e425309b3e90db4aa755
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-06-07 21:04:22 +0200

    fix media-collection query

--- server/src/routes/media-collections.ts
+++ server/src/routes/media-collections.ts
@@ -20,7 +20,7 @@ export async function continueWatching (ctx: Context) {
     nextEpisodes = await Episode.createQueryBuilder('episodes')
       .where(new Brackets(qb => {
           qb
-            .where('episodes.id IN (:...ids)', { ids: seriesWatched.map(e => e.movie) })
+            .where('episodes.movie IN (:...ids)', { ids: seriesWatched.map(e => e.movie) })
             .andWhere('episodes.watched < 95')
         }
       ))
