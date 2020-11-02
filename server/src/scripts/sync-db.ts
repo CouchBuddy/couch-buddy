ca1b4f3556dfc2ce2a4e6cf1be21b7897a1f2c4a
Luca Faggianelli <luca.faggianelli@global.ntt>
2020-11-02 13:15:20 +0100

    🐛 fix sync-db script

--- server/src/scripts/sync-db.ts
+++ server/src/scripts/sync-db.ts
@@ -2,12 +2,11 @@ import boot from './boot'
 import { getConnection } from 'typeorm'
 
 async function main (bar: any) {
-  getConnection().synchronize(false)
+  await getConnection().synchronize(false)
 }
 
 if (require.main === module) {
   (async function () {
     await main(await boot())
-    process.exit(0)
   })()
 }
